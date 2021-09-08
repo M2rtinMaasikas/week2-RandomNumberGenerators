8fc9fae82d64b160c198267a796c8612ae17d6b9
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:27:08 +0300

    first commit

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus oseleb 2 mängijat
+            //iga mängija viskab täringut (1-6);
+            //programm kuvab nende visete tulemust ja kuvab ka, kes on võitja
+            //ehk kes viskas suuremat
+
+            Random rnd = new Random();
+
+            string PlayerOne = "Harry Potter,";
+            string PlayerTwo = "lkqwe";
+            
+                int HarryThrow = rnd.Next(1, 7);
+            int lkqweThrow = rnd.Next(1, 7);
+
+            Console.WriteLine($" {PlayerOne} threw {HarryThrow};");
+            Console.WriteLine($" {PlayerTwo} threw {lkqweThrow};");
+
+
+            if (HarryThrow > lkqweThrow)
+            {
+                Console.WriteLine($"{PlayerOne} wins!");
+            }
+            else if (HarryThrow < lkqweThrow)
+            {
+                Console.WriteLine($"{PlayerTwo} wins!");
+            }
+            else
+            {
+                Console.WriteLine("draw! let them try again!");
+            }
+
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(5, 16);
+
+            //programm genereerib juhuslikku numbrit
+            //programm kontrollib, kas genereeritud number on 
+            //suuerm kui 5
+            //kui number on suurem kui 5 siis konsool kuvab
+            //juhsulik number on MyRandomNumber, see on väiksem
+            //kui 5
+            //kui number on 5, siis konsool kuvab "juhuslik number on 5"
+
+
+
+            Console.WriteLine($"arvuti genereeris: {MyRandomNumber}");
+            if(MyRandomNumber > 5)
+            {
+                Console.WriteLine($"juhuslik number {MyRandomNumber}, see on suurem, kui 5");
+            }
+            else if (MyRandomNumber < 5)
+            {
+                Console.WriteLine($"juhuslik number {MyRandomNumber}, see on väiksem, kui 5");
+            }
+            
+            else 
+            {
+                Console.WriteLine("Juhuslik nuber ongi 5");
+            }
+
+
+
+
+        }
+    }
+}
